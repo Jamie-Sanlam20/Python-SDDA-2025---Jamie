37c698b0cb36bd256b52cab1c208edc131608448
Jamie-Sanlam20 <jamie.isaacs@sanlam.co.za>
2025-03-03 15:37:28 +0200

    Add naming variable

---
+++ Day_09/06_naming_variable.py
@@ -0,0 +1,19 @@
+# Variable names
+
+# Syntax Rules - will cause syntax errors
+# 1. Avoid keywords
+# cannot use if, else, elif, while, is, for -> as variable names
+# 2. Don't overwrite built-in functions
+# e.g., sum = 2 (sum is already a function)
+# 3. Cannot start with number
+# e.g., 1t = ❌
+# e.g., t1 = ✅
+# 4. Cannot use special sign except _
+# e.g., num$ = ❌
+# e.g., num_1 = ✅
+
+# Good Practice
+# 1. Use snake_case for general variable names
+# 2. Should be descriptive (but keep it short)
+# 3. CONSTANT_CASE (variable should be in capital letters)
+# e.g., PI = 3.14 (doesn't change)
