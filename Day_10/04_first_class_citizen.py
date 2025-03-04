a917e739913402bfd49eeeaae93b51f3c5cddae9
Jamie-Sanlam20 <jamie.isaacs@sanlam.co.za>
2025-03-04 18:12:17 +0200

    Update first class citizen + lambda map filter + map filter ex

--- Day_10/04_first_class_citizen.py
+++ Day_10/04_first_class_citizen.py
@@ -1,3 +1,60 @@
+# In python
+# Function treated as first class citizen (value)
+
+
+# Rules (for variables)
+# 1. Assign to variable
+# 2. Pass as argument
+# 3. Returned
+
+# Rules (functions)
+# 1. Function: Assign to variable
+# 2. Function: Pass as argument
+# 3. Function: Returned
+
+# 1. Variable: Assign to variable
+
+# x = 1
+
+
+# # 1. Function: Assign to variable
+
+# def greet(name):
+#     return f"Hi, {name}"
+
+
+# print(type(greet))  # <class 'function'>
+# # print(type(greet("Jevan")))
+
+# y = greet  # function
+
+# print(y("Jamie"))  # Hi, Jamie
+
+
+# 2. Function: Pass as argument
+
+
+def say_hello():
+    return "Hello, "
+
+
+# # 2. Function: Pass as argument - say_hello (function) is passed as argument
+def greeting(hello_msg, name):
+    print(
+        hello_msg() + name
+    )  # hello_msg acts as placeholder to show that function datatype is parsed
+
+
+greeting(say_hello, "Python!")
+
+# Functional languages - F#, Haskell, Scala - Recursion
+
+# print(say_hello())
+
+
+# 3. Function: Returned
+
+
 def f1():
     def f2():
         return "Hi 😀👋"
@@ -5,7 +62,18 @@ def f1():
     return f2  # -> returning function
 
 
+# Make it print - "Hi 😀👋"
 # f1 is returning f2 -> call f1 --> == f2, then () to call function
 
+# Sol 1 -> assign to variable x
+x = f1()  # f2
+print(x())  # f2()
 
+# Sol 2
 print(f1()())  # --> to print Hi
+
+# Summary:
+# Rules
+# 1. Function: Assign to variable
+# 2. Function: Pass as argument
+# 3. Function: Returned
--- Day_10/05_lambda_map_filter.py
+++ Day_10/05_lambda_map_filter.py
@@ -1,14 +1,22 @@
-# convert to lambda expression if function body 1 line
+# Lambda expression/ Lambda Function: Function one liner
 
 
 def greet(name):
     return f"Hi, {name}"
 
 
+# convert to lambda expression if function body 1 line
+
 greet1 = lambda name: f"Hi, {name}"  # doesn't like us naming lambda
 
 print(greet("Jevan"))
 print(greet1("Jevan"))
+
+# Lambda expression
+# 1. Anonymous - nameless function
+# 2. One liner
+# 3. Implicit return (automatically)
+
 
 # lambda name -> parameter
 # f"Hi, {name} -> return statement (do not have to say return - implicit return)
@@ -20,16 +28,44 @@ def add(n1, n2):
 
 add1 = lambda n1, n2: n1 + n2
 
+print(add1(9, 8))
+
 # Why is it useful?
+
+player_stats = [10, 30, 60]
+boosted_stats = map(lambda x: x * 2, player_stats)
+print(boosted_stats, list(boosted_stats))
 
 # map & filter
 # map -> function -> takes another function as argument
 # map -> Higher order functions
 
+boosted_stats = map(lambda x: x * 2, [10, 30, 60])
+
+dbl = lambda x: x * 2
+boosted_stats = map(dbl, [10, 30, 60])
+
+print(boosted_stats, list(boosted_stats))
+
+
+def dbl1(x):
+    return x * 2
+
+
+boosted_stats = map(dbl1, [10, 30, 60])
+print(boosted_stats, list(boosted_stats))
+
+
+boosted_stats = map(lambda x: x * 2, player_stats)
+print(boosted_stats, list(boosted_stats))
+
 # map calls function to each element
 # dbl (10) -> first
 # dbl (30) -> second
 # dbl (60) -> third
+
+# 1. def - Reuse else where
+# 2. lambda - one time use, concise
 
 # answers gets passed into list
 
@@ -62,3 +98,8 @@ print(player_stats)  # Original list isn't affected
 # 1. len(input_list) >= len(output_list) -> removing some values
 # 2. Input data_type == Output data_type -> Cannot transform datatype ❌
 # 3. Doesn't affect Input (original) list -> Output is a copy
+
+player_stats = [10, 30, 60]
+result = filter(gt1, player_stats)
+print(result, list(result))
+print(player_stats)
