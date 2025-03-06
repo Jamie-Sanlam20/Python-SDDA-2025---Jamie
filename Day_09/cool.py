19c0c98dea83a1d7c1e7038533518f26fb6c58d2
Jamie-Sanlam20 <jamie.isaacs@sanlam.co.za>
2025-03-06 17:43:00 +0200

    Update formatting output + cool + fun.py

--- Day_09/cool.py
+++ Day_09/cool.py
@@ -1,5 +1,25 @@
 def to_upper_case(text):
+    """
+    This is beautiful function that returns uppercase value with wave emoji
+    """
     return text.upper() + " 👋"
 
 
-print(to_upper_case("Jamie"))
+#  Only when cool.py execute -> Jamie
+# dunder variables
+
+
+def main():
+    # Specific cool.py code
+    print(__name__)
+    print(to_upper_case("Jamie"))
+
+
+if __name__ == "__main__":
+    main()
+
+# Two values for __name__:
+# Self execution    -  __name__ -> __main__
+# From another file -  __name__ -> file name
+
+# sum()
--- Day_09/fun.py
+++ Day_09/fun.py
@@ -1,7 +1,34 @@
+# from cool import to_upper_case
+
+# # from file name import function name
+
+# print(to_upper_case("Siya"))
+
+# # it's reprinting Jamie -> print statement from cool file is running also
+
 from cool import to_upper_case
 
-# from file name import function name
 
-print(to_upper_case("Siya"))
+def powerful(n):
+    """Squares given number
 
-# it's reprinting Jamie -> print statement from cool file is running also
+    Args:
+        n (int): provide number
+
+    Returns:
+        int: square of the value
+    """
+    return n * n
+
+
+print(powerful(5))
+
+print(__name__)  # fun | __main__
+
+
+def main():
+    print(to_upper_case("Siya"))
+
+
+if __name__ == "__main__":
+    main()
